2550d06a72709c8347799b192ab99fa1cf9e7d98
Angelo Bovino <angelo.bovino@outlook.com>
2018-01-20 22:19:36 -0500

    Adding routes and templates

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article One',
+			'body': 'lorem ipsum dolar sit amet',
+			'author': 'Angelo Bovino',
+			'create_date': '01-20-2018'
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'Two lorem ipsum dolar sit amet',
+			'author': 'Benito Bovino',
+			'create_date': '01-19-2018'
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'lorem ipsum dolar sit amet',
+			'author': 'Alisa Bovino',
+			'create_date': '01-20-2018'
+		}
+	]
+
+	return articles
